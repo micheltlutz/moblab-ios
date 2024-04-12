0e8ec49b0cbee4aa1ce83309ef3217dabab13faf
Michel Anderson Lutz Teixeira <michel_lutz@icloud.com>
2024-04-12 20:17:18 -0300

    first commit

---
+++ MobLab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MobLab
+//
+//  Created by Michel Anderson Lutz Teixeira on 12/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
